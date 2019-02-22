1a1bd8593978d8ac992a528e131eb38cb63788de
tobetakoyaki <culixewscheptf.26@gmail.com>
2019-02-22 19:47:21 +0900

    アニメーション機能の追加

--- Earth-Mars.py
+++ Earth-Mars.py
@@ -1,6 +1,10 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
+font = {'family':'YuGothic'}
+matplotlib.rc('font',**font)
 
 # define constant
 Period_Earth = 1.00000 # 年
@@ -9,9 +13,8 @@ Distance_Earth = 1.4960 # 億km
 Distance_Mars = 2.2794 # 億km
 
 # Angle
-def planet_angle(t,T):
-    alpha = 2 * np.pi * t / T
-    return alpha - 2 * np.pi * np.floor(alpha / (2*np.pi))
+def angle_translator(theta):
+    return 2 * np.pi * (theta - np.floor(theta))
 
 def polar(r, theta):
     return np.array([r * np.cos(theta), r * np.sin(theta)])
@@ -19,33 +22,31 @@ def polar(r, theta):
 
 def angle_on_earth(t):
     # Location of two planets
-    thetaE = planet_angle(t, Period_Earth)
-    # thetaM = planet_angle(t, Period_Mars)
+    thetaE = angle_translator(2 * np.pi * t/Period_Earth)
+    thetaM = angle_translator(2 * np.pi * t/Period_Mars)
     Location_Earth = polar(Distance_Earth, thetaE)
-    #Location_Mars = polar(Distance_Mars, thetaM)
-    #Location_diff = Location_Mars - Location_Earth
+    Location_Mars = polar(Distance_Mars, thetaM)
+    Location_diff = Location_Mars - Location_Earth
     # Angle
-    #phi = np.arctan2(Location_diff[0],Location_diff[1])
-    #return 0.5 * np.pi - thetaE + phi
-    return Location_Earth
+    return np.arctan2(Location_diff[1],Location_diff[0])
 
+time = [t/100 for t in range(0,1001)]
+Xdat = []
+Ydat = []
 fig = plt.figure()
-X, Y = [], []
+ax = fig.add_subplot(111)
+ax.set_xlim(-1.5,1.5)
+ax.set_ylim(-1.5,1.5)
+ims = []
 
-def update(i):
-    if i!= 0:
-        plt.cla()
-    
-    L = angle_on_earth(i)
-    X.append(L[0])
-    Y.append(L[1])
-    plt.xlim(-1.5,1.5)
-    plt.ylim(-1.5,1.5)
-    plt.plot(X, Y, "r")
+for t in time:
+  angle = angle_on_earth(t)
+  Xdat.append(np.cos(angle))
+  Ydat.append(np.sin(angle))
+  Xdat = Xdat[-10:]
+  Ydat = Ydat[-10:]
+  im = ax.plot(Xdat,Ydat)
+  ims.append(im)
 
-ani = animation.FuncAnimation(fig, update, interval=10, frames=132)
-ani.save("Sample.gif", writer='imagemagick')
-# time = np.array(range(0,int(1e+4)))
-# angle = np.array([angle_on_earth(t) for t in time])
-# plt.plot(time, angle)
-# plt.show()
+ani = animation.ArtistAnimation(fig, ims, interval=100)
+ani.save("test.gif", writer="imagemagick") 
